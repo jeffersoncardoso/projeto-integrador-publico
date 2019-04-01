4802b2bd33fabe1fbe08b80b85132ee4cd8276d2
Jefferson Cardoso <jefferson.mcardoso@gmail.com>
2019-04-01 20:35:48 -0300

    Criando ENV e instalando axios

---
+++ src/env.js
@@ -0,0 +1,13 @@
+export const ENV = {
+    'api.sistema.cadastrar' : '',
+    'api.sistema.listar' : '/json/Sistemas.json',
+    'api.sistema.editar' : '',
+    'api.sistema.excluir' : '',
+    'api.sistema.buscar' : '',
+
+    'api.utilitario.cadastrar' : '',
+    'api.utilitario.listar' : '/json/Utilitarios.json',
+    'api.utilitario.editar' : '',
+    'api.utilitario.excluir' : '',
+    'api.utilitario.buscar' : 'https://baconipsum.com/api/?type=meat-and-filler',
+}
