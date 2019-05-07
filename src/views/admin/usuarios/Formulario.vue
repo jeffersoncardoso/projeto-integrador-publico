0243838d6bc72f782fd6101b745ef4969caf250d
Jefferson Martins Cardoso <0190690@feevale.br>
2019-05-06 21:20:17 -0300

    Ajustes no avisos e crud usuários

--- src/views/admin/usuarios/Formulario.vue
+++ src/views/admin/usuarios/Formulario.vue
@@ -5,12 +5,12 @@
                 <v-text-field v-model="usuario.cpf" label="CPF" placeholder="Digite o CPF do usuário" required autofocus></v-text-field>
             </v-flex>
             <v-flex lg7 md7 sm7>
-                <v-text-field v-model="usuario.nome" label="Nome" disabled></v-text-field>
+                <v-text-field label="Nome" disabled></v-text-field>
             </v-flex>
         </v-layout>
         <v-layout row wrap>
             <v-flex lg5 md5 sm5>
-                <v-autocomplete v-model="usuario.perfil" :items="['Administrador', 'Mídia']" placeholder="Selecione o Perfil" clearable></v-autocomplete>
+                <v-autocomplete v-model="usuario.idperfil" item-value="id" item-text="nome" :items="perfis" placeholder="Selecione o Perfil" clearable></v-autocomplete>
             </v-flex>
         </v-layout>
         <div class="text-xs-right">
@@ -27,9 +27,37 @@ export default {
         return {
             usuario: {
                 cpf: '',
-                nome: '',
-                perfil: ''    
+                idperfil: ''
+            },
+            perfis: []
+        }
+    },
+    created() {
+        this.$http.get(ENV['api.perfil']).then((result) => {
+            this.perfis = result.data
+        })
+    },
+    methods: {
+        getId() {
+            return this.$router.currentRoute.params.id
+        },
+        estaEditando() {
+            return (this.$router.currentRoute.name == 'admin.usuarios.editar');
+        },
+        save() {
+            let promise
+            if(this.estaEditando()) {
+                promise = this.$http.put(ENV['api.usuario'] + this.getId(), this.usuario)
+            }else{
+                promise = this.$http.post(ENV['api.usuario'], this.usuario)
             }
+
+            promise.then((response) => { 
+                this.aviso = response.data
+                this.$router.push({'name': 'admin.usuarios.listar'})
+            }).catch(error => {
+                console.log(error)
+            })
         }
     },
     watch: {    
@@ -38,8 +66,8 @@ export default {
                 this.$http.get(ENV['api.buscar-cpf'] + '/' + this.usuario.cpf, {
                     headers: { Authorization: 'Basic OmUzY2RjOTBjNmJhMTc1ZThjZWNiZDEwMDI0OTAzNTZl' }
                 }).then(result => {
-                    if(result.data)
-                        this.usuario.nome = result.data.nome
+                    //if(result.data)
+                        //this.usuario.nome = result.data.nome
                 })
             }
         }
