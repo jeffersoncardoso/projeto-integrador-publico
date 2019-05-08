6a00020b8e3b8bf7bcfdadf0b86122fc677b0023
Jefferson Cardoso <jefferson.mcardoso@gmail.com>
2019-05-07 22:31:22 -0300

    Ajustando env

--- src/env.js
+++ src/env.js
@@ -5,7 +5,7 @@ export const ENV = {
     'api.usuario' : 'https://portalservicos.jeffersoncardoso.info/api/usuario/',
     'api.sistema' : 'https://portalservicos.jeffersoncardoso.info/api/sistema/',
     'api.utilitario' : 'https://portalservicos.jeffersoncardoso.info/api/utilitario/',
-    'api.buscar-funcionario' : 'https://portalservicos.jeffersoncardoso.info/api/funcionario/params?token=jBVJJWlc2wWWGcAye0GdsIdHFnqcv6pB'
+    'api.buscar-funcionario' : 'https://portalservicos.jeffersoncardoso.info/api/funcionario/params?token=jBVJJWlc2wWWGcAye0GdsIdHFnqcv6pB',
     'api.buscar-cpf' : 'https://sistemas.canoas.rs.gov.br/ldap-api/funcionario',
     'firebase' : 'key=AAAAijerZYI:APA91bF0KPxwb4OGgoumMciW1cKElrGDj4SMLuUWDKJwM0b8Sctu8-o_fJ7CSkCODlrZVaCQ5qRWD7R3RIL38YphUAkXiee9t2ckPTTil5ikvUx3awrnpCCPAwr1PCRU6IQvqmU7QDb_'
 }
